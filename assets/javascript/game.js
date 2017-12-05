7fedc8bb6b0bf34c27c62d5b9f43b5e8219b5e12
Robert Castellano <castellanorob@gmail.com>
2017-12-04 18:17:55 -0600

    Another Hangman Update

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+
+
+// Array that holds the names of big cats
+
+var cats = ["lion", "tiger", "theetah", "leopard", "cougar", "jaguar", "puma"];
+
+// Variables for wins, losses
+
+var wins = 0;
+var losses = 0;
+
+// Function that logs key events
+
+document.onkeyup = function() {
+	var userguess = String.fromCharCode(event.keyCode).toLowerCase();
+}
+
+console.log(userguess)
+
+// Function that selects an item from the array for the user to guess
+
+// Function that displays underscores for the letters before the user begins guessing
+
+// Function that displays letters for correct guesses
+
+// Function that logs and displays letters for incorrect guesses
+
+// Function that displays the number of remaining guesses
+
+// 
